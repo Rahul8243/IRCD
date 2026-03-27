bb83723a7d9ea4c467f7c22510fd42f7f508aae5
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-03-27 11:23:01 +0530

    lab assignment question

---
+++ lab-7/question-4.py
@@ -0,0 +1,47 @@
+# Q4: 2D Prefix Sum: Matrix Region Sum  
+# Given an m x n matrix, preprocess it so that you can answer rectangle sum queries 
+# efficiently.  
+# Each query provides:  
+# (row1,col1,row2,col2) 
+# Return the sum of elements inside the rectangle. 
+
+class NumMatrix:
+
+    def __init__(self, matrix):
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        self.prefix = [[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+
+                self.prefix[i][j] = (
+                    matrix[i-1][j-1]
+                    + self.prefix[i-1][j]
+                    + self.prefix[i][j-1]
+                    - self.prefix[i-1][j-1]
+                )
+
+    def sumRegion(self, row1, col1, row2, col2):
+
+        return (
+            self.prefix[row2+1][col2+1]
+            - self.prefix[row1][col2+1]
+            - self.prefix[row2+1][col1]
+            + self.prefix[row1][col1]
+        )
+
+
+matrix = [
+ [3,0,1,4,2],
+ [5,6,3,2,1],
+ [1,2,0,1,5],
+ [4,1,0,1,7],
+ [1,0,3,0,5]
+]
+
+obj = NumMatrix(matrix)
+
+print(obj.sumRegion(2,1,4,3))
