868ae5031d50631592f94d594aea8b891895c1aa
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-04-08 11:14:57 +0530

    find gcd

---
+++ lab-10/find_(GCD)/Brute_force_method.py
@@ -0,0 +1,12 @@
+def gcd_basic(a, b):
+    gcd = 1 
+    
+    
+    for i in range(1, min(a, b) + 1):
+        if a % i == 0 and b % i == 0:
+            gcd = i 
+            
+    return gcd
+
+print(gcd_basic(12,18))
+
